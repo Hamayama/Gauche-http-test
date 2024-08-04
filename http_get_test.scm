8d105f0d4128e2a3da4890d334aa9bf1c4bdfed6
Hamayama <fkyo0985@gmail.com>
2024-08-04 10:30:29 +0900

    Fix GitHib Actions

--- http_get_test.scm
+++ http_get_test.scm
@@ -1,7 +1,26 @@
 (use rfc.tls)
-(print "tls-ca-bundle-path=" (tls-ca-bundle-path))
-
 (use rfc.http)
-(receive (status header-list body)
-    (http-get "www.microsoft.com" "/" :secure #t)
-  (print "http status=" status))
+(use gauche.process)
+
+(define (http-get-test)
+  (print "tls-ca-bundle-path=" (tls-ca-bundle-path))
+  (guard (ex [else (print ex)])
+    (receive (status header-list body)
+        (http-get "www.microsoft.com" "/" :secure #t)
+      (print "http status=" status))))
+
+(print)
+(print "== HTTP TEST ==")
+(print)
+
+(http-get-test)
+(print)
+
+(tls-ca-bundle-path 'system)
+(http-get-test)
+(print)
+
+(tls-ca-bundle-path #"~(process-output->string \"which openssl\")/cert.pem")
+(http-get-test)
+(print)
+
