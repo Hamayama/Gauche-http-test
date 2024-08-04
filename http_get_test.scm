f7b72b0f707b326ffb4787226a863f65ef6b8948
Hamayama <fkyo0985@gmail.com>
2024-08-04 09:59:12 +0900

    Fix GitHib Actions

--- http_get_test.scm
+++ http_get_test.scm
@@ -1,4 +1,7 @@
+(use rfc.tls)
+(print "tls-ca-bundle-path=" (tls-ca-bundle-path))
+
 (use rfc.http)
 (receive (status header-list body)
     (http-get "www.microsoft.com" "/" :secure #t)
-  (print status))
+  (print "http status=" status))
